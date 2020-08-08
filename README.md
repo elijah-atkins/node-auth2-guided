b13d62bd8c9f4c332d9c80a72069502e8f2c27b9
Elijah Atkins <elijahorama@gmail.com>
2020-08-08 10:10:51 -0600

    project notes

--- README.md
+++ README.md
@@ -15,3 +15,11 @@ Guided project for **Node Auth 2** Module.
 - [ ] type `npm run server` to start the API.
 
 Please follow along as the instructor adds support for `JSON Web Tokens (JWT)` to the API.
+
+## Files with notes:
+
+- ./auth/auth-router.js
+- ./auth/check-role-middleware.js
+- ./auth/restricted-middleware.js
+- ./config/secrets.js
+- ./users/users-router.js
--- users/users-router.js
+++ users/users-router.js
@@ -2,13 +2,22 @@ const router = require("express").Router();
 
 const Users = require("./users-model.js");
 const restricted = require("../auth/restricted-middleware.js");
+const checkRole = require('../auth/check-role-middleware.js');
 
-router.get("/", restricted, (req, res) => {
+//----------------------------------------------------------------------------//
+// This is where we use checkRole(). "1" is the ID for the "Admin" role. Calling
+// checkRole(1) returns a middleware function that checks to see 1) if the
+// decoded token on the req object has a "role" property, and 2, if that
+// property has the value of "1". 
+//----------------------------------------------------------------------------//
+router.get("/", restricted, checkRole(1), (req, res) => {
   Users.find()
     .then(users => {
       res.status(200).json(users);
     })
     .catch(err => res.send(err));
 });
+
+
 
 module.exports = router;
